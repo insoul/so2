c118e7c4c96bed8d1c7c6717b25eb17b0d5d5813
insoul <insoo.jung+github@gmail.com>
2022-10-20 17:12:42 +0900

    shell_ssh_command

--- lib/shell_option.rb
+++ lib/shell_option.rb
@@ -1,15 +1,33 @@
 require 'option'
+require 'i18n'
 
 class ShellOption < Option
   OPTION_NAMES = %w{
     user server kube
+    shell_ssh_command
   }
   def self.option_names; OPTION_NAMES end
+
+  def address
+    if @options.user
+      "#{@options.user}@#{@options.server}"
+    else
+      @options.server
+    end
+  end
 
   def run
     if kube?
       svr = kube_servers.last
       cmd = "kubectl exec -n #{@kube['namespace']} -it #{svr} -c #{@kube['container'] || 'main'} -- /bin/bash"
+      puts cmd
+      system cmd
+    else
+      if @options.shell_ssh_command
+        cmd = I18n.interpolate(@options.shell_ssh_command, address: address)
+      else
+        cmd = "ssh #{address}"
+      end
       puts cmd
       system cmd
     end
--- subcommands/shell.rb
+++ subcommands/shell.rb
@@ -5,14 +5,13 @@ options = ShellOption.new(SO2_CONFIG['default'])
 command_options = OpenStruct.new
 
 opts = OptionParser.new do |opts|
-  opts.on("-s", "--server HOSTNAME", "Target server to upload") do |f|
-    command_options.server = f
+  opts.on("-s", "--stage STAGE", "Target server to upload") do |f|
+    command_options.stage = f
   end
 end
 
 opts.parse!(ARGV)
-stage = opts.default_argv.first
-stage = 'default' if stage.nil? || stage.start_with?('-')
+stage = command_options.stage || 'default'
 options.set(SO2_CONFIG[stage])
 options.set(command_options)
 puts ""
